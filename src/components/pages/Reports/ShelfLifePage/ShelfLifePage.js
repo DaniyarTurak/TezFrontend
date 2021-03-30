2a88f42a02829082e510fdef1b72a16475961810
Vyacheslav <fyodorov_vd@list.ru>
2021-03-30 13:20:12 +0600

    grzeg3

--- src/components/pages/Reports/ShelfLifePage/ShelfLifePage.js
+++ src/components/pages/Reports/ShelfLifePage/ShelfLifePage.js
@@ -8,10 +8,11 @@ import moment from 'moment';
 
 export default function ShelfLifePage() {
   const [expdates, setExpdates] = useState([]);
-  const arrays = [];
+  const [datesExcel, setDatesExcel] = useState([]);
   const [isLoading, setLoading] = useState(false);
   const [isExcelLoading, setExcelLoading] = useState(false);
   const [code, setCode] = useState("");
+
   useEffect(() => {
     getExpireDates();
   }, []);
@@ -28,11 +29,13 @@ export default function ShelfLifePage() {
     Axios.get("/api/report/expire_date")
       .then((res) => res.data)
       .then((expiredates) => {
-        arrays.push(expiredates[0].rep_exp_date.array3);
-        arrays.push(expiredates[0].rep_exp_date.array6);
-        arrays.push(expiredates[0].rep_exp_date.array9);
-        arrays.push(expiredates[0].rep_exp_date.array12);
-        setExpdates(arrays);
+        let newarr = [];
+        newarr.push(expiredates[0].rep_exp_date.array3);
+        newarr.push(expiredates[0].rep_exp_date.array6);
+        newarr.push(expiredates[0].rep_exp_date.array9);
+        newarr.push(expiredates[0].rep_exp_date.array12);
+        setDatesExcel(newarr);
+        setExpdates(newarr);
         setLoading(false);
         setCode(expiredates[0].rep_exp_date.code);
       })
@@ -40,26 +43,40 @@ export default function ShelfLifePage() {
         ErrorAlert(err);
         setLoading(false)
       });
-  };;
+  };
+
+  console.log(datesExcel[0]);
 
   const getShelfLifeExcel = () => {
     setExcelLoading(true);
-    let arr3 = expdates[0];
-    let arr6 = expdates[1];
-    let arr9 = expdates[2];
-    let arr12 = expdates[3];
-    arr3.forEach(element => {
-      element.dt = moment(element.dt).format('L')
-    });
-    arr6.forEach(element => {
-      element.dt = moment(element.dt).format('L')
-    });
-    arr9.forEach(element => {
-      element.dt = moment(element.dt).format('L')
-    });
-    arr12.forEach(element => {
-      element.dt = moment(element.dt).format('L')
-    });
+    let arr3 = [];
+    let arr6 = [];
+    let arr9 = [];
+    let arr12 = [];
+
+    if (datesExcel[0]) {
+      datesExcel[0].forEach((e) => {
+        arr3.push({ ...e, dt: moment(e.dt).format('L') })
+      })
+    };
+    
+    if (datesExcel[1]) {
+      datesExcel[1].forEach((e) => {
+        arr6.push({ ...e, dt: moment(e.dt).format('L') })
+      })
+    };
+
+    if (datesExcel[2]) {
+      datesExcel[2].forEach((e) => {
+        arr9.push({ ...e, dt: moment(e.dt).format('L') })
+      })
+    };
+
+    if (datesExcel[3]) {
+      datesExcel[3].forEach((e) => {
+        arr12.push({ ...e, dt: moment(e.dt).format('L') })
+      })
+    };
 
     Axios({
       method: "POST",
@@ -84,7 +101,6 @@ export default function ShelfLifePage() {
   };
 
   return (
-
     <Grid container spacing={3}>
       {expdates.length > 0 && <Fragment>
         {periodProps.map((period, i) => (expdates.length !== 0 &&
