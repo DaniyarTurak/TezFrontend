5c8519984de317bea1a1ee2966bd375647ed472a
Vyacheslav <fyodorov_vd@list.ru>
2021-03-31 08:13:03 +0600

    отстаток на кассе v4

--- src/components/pages/Reports/ReportCashboxState/ReportCashboxState.js
+++ src/components/pages/Reports/ReportCashboxState/ReportCashboxState.js
@@ -185,7 +185,7 @@ export default function ReportCashboxState({ company, holding }) {
                   Время (открытия / закрытия)
                 </StyledCell>
                 <StyledCell align="center">
-                  <Tooltip title={<p style={{ padding: "0px", fontSize: ".875rem" }}>Остаток наличности в кассе</p>}>
+                  <Tooltip title={<p style={{ padding: "0px", fontSize: ".875rem" }}>Остаток в кассе на текущий момент</p>}>
                     <span style={{ cursor: "pointer" }}>Остаток в кассе*</span>
                   </Tooltip>
                 </StyledCell>
