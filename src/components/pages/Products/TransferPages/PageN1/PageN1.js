2745ef7db56b654e7c88aea9de760c28713188f2
Vyacheslav <fyodorov_vd@list.ru>
2021-06-03 14:25:20 +0600

    перемещение штучных товаров

--- src/components/pages/Products/TransferPages/PageN1/PageN1.js
+++ src/components/pages/Products/TransferPages/PageN1/PageN1.js
@@ -265,7 +265,9 @@ export default function PageN1({
           attributes: productSelectValue.attributes,
           SKU: null,
           newprice: newPriceCheck ? detail.priceto : detail.price,
-          pieceprice: detail.pieceprice
+          piece: detail.piece,
+          pieceprice: detail.pieceprice,
+          unitsprid: detail.unitsprid
         },
       ],
     };
