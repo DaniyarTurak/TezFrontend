f6757b59876281c874f881272d238d5f3d5177f4
Perizat Sabitbek <sonepiko@gmail.com>
2021-12-02 08:55:46 +0600

    add table imports

--- src/components/pages/Reports/ReportCashboxState/DebtDetail.js
+++ src/components/pages/Reports/ReportCashboxState/DebtDetail.js
@@ -1,7 +1,14 @@
 import React, {useState, Fragment} from 'react';
 import Modal from '@material-ui/core/Modal';
 import Box from '@material-ui/core/Box';
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 
   const useStyles = makeStyles(() => ({
     modal: {
@@ -19,6 +26,23 @@ import { makeStyles } from "@material-ui/core/styles";
       pb: 3,
     },
   }));
+  const StyledCell = withStyles((theme) => ({
+    head: {
+      backgroundColor: "#17a2b8",
+      color: theme.palette.common.white,
+      fontSize: ".875rem",
+    },
+    body: {
+      fontSize: ".875rem",
+    },
+    footer: {
+      color: theme.palette.common.black,
+      fontSize: ".875rem",
+      fontWeight: "bold",
+    },
+  }))(TableCell);
+
+
 function DebtDetail({rep}) {
     const [openModal, setOpenModal] = useState(false)
     const classes = useStyles();
@@ -49,7 +73,7 @@ function DebtDetail({rep}) {
           onClose={closeModalHandler}
         >
           <Box className={classes.modal}>
-            <h2 id="child-modal-title">hello</h2>
+            <h6 id="child-modal-title">hello</h6>
             <p id="child-modal-description">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit.
             </p>
