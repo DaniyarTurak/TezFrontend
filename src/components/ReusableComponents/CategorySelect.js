de5d64be07f55d4a0367b96d70451389409d279c
piko0101 <sonepiko@gmail.com>
2022-02-08 10:31:33 +0600

    uninstall old treeview component

--- src/components/ReusableComponents/CategorySelect.js
+++ src/components/ReusableComponents/CategorySelect.js
@@ -1,60 +1,60 @@
-import React, { useState, Fragment, useEffect } from 'react';
-import DropdownTreeSelect from "react-dropdown-tree-select";
-import Axios from '../../../node_modules/axios/index';
-import "./CategorySelect.css";
+// import React, { useState, Fragment, useEffect } from 'react';
+// import DropdownTreeSelect from "react-dropdown-tree-select";
+// import Axios from '../../../node_modules/axios/index';
+// import "./CategorySelect.css";
 
 
 
-function CategorySelect({onCategoryChange, setCategory}) {
+// function CategorySelect({onCategoryChange, setCategory}) {
 
-  useEffect(() => {
-    getCategories();
-    assignObjectPaths(categories);
-  }, [])
+//   useEffect(() => {
+//     getCategories();
+//     assignObjectPaths(categories);
+//   }, [])
 
-  const [categories, setCategories] = useState([])
+//   const [categories, setCategories] = useState([])
 
 
-  const getCategories = () => {
-    Axios.get("/api/categories/get_categories")
-      .then((res) => res.data)
-      .then((data) => {
-       let updatedData = data;
+//   const getCategories = () => {
+//     Axios.get("/api/categories/get_categories")
+//       .then((res) => res.data)
+//       .then((data) => {
+//        let updatedData = data;
      
-       setCategories(updatedData)
-      })
-      .catch((err) => console.log(err))
-  }
+//        setCategories(updatedData)
+//       })
+//       .catch((err) => console.log(err))
+//   }
 
-  const onChange = (currentNode, selectedNodes) => {
-    setCategory(currentNode)
-    console.log(currentNode)
-  };
+//   const onChange = (currentNode, selectedNodes) => {
+//     setCategory(currentNode)
+//     console.log(currentNode)
+//   };
   
-  const assignObjectPaths = (obj, stack) => {
-    Object.keys(obj).forEach(k => {
-      const node = obj[k];
-      if (typeof node === "object") {
-        node.path = stack ? `${stack}.${k}` : k;
-        assignObjectPaths(node, node.path);
-      }
-    });
-  };
+//   const assignObjectPaths = (obj, stack) => {
+//     Object.keys(obj).forEach(k => {
+//       const node = obj[k];
+//       if (typeof node === "object") {
+//         node.path = stack ? `${stack}.${k}` : k;
+//         assignObjectPaths(node, node.path);
+//       }
+//     });
+//   };
   
 
-  return (
-    <Fragment>
-      <DropdownTreeSelect 
-        data={categories} 
-        onChange={onChange}
-        className="treeview-selector" 
-        mode={"radioSelect"}
-        texts={{label: 'Категории'}}  
-      />
+//   return (
+//     <Fragment>
+//       <DropdownTreeSelect 
+//         data={categories} 
+//         onChange={onChange}
+//         className="treeview-selector" 
+//         mode={"radioSelect"}
+//         texts={{label: 'Категории'}}  
+//       />
 
 
-    </Fragment>
-  )
-}
+//     </Fragment>
+//   )
+// }
 
-export default CategorySelect;
+// export default CategorySelect;
