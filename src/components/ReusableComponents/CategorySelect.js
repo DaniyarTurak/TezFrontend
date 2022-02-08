75e1b96b4974c9a557a5aff6199f73f6eebbff4c
piko0101 <sonepiko@gmail.com>
2022-02-08 10:28:05 +0600

    create new category select

--- src/components/ReusableComponents/CategorySelect.js
+++ src/components/ReusableComponents/CategorySelect.js
@@ -1,60 +1,9 @@
-import React, { useState, Fragment, useEffect } from 'react';
-import DropdownTreeSelect from "react-dropdown-tree-select";
-import Axios from '../../../node_modules/axios/index';
-import "./CategorySelect.css";
+import React from 'react';
 
-
-
-function CategorySelect({onCategoryChange, setCategory}) {
-
-  useEffect(() => {
-    getCategories();
-    assignObjectPaths(categories);
-  }, [])
-
-  const [categories, setCategories] = useState([])
-
-
-  const getCategories = () => {
-    Axios.get("/api/categories/get_categories")
-      .then((res) => res.data)
-      .then((data) => {
-       let updatedData = data;
-     
-       setCategories(updatedData)
-      })
-      .catch((err) => console.log(err))
-  }
-
-  const onChange = (currentNode, selectedNodes) => {
-    setCategory(currentNode)
-    console.log(currentNode)
-  };
+function CategorySelect() {
+  return 
   
-  const assignObjectPaths = (obj, stack) => {
-    Object.keys(obj).forEach(k => {
-      const node = obj[k];
-      if (typeof node === "object") {
-        node.path = stack ? `${stack}.${k}` : k;
-        assignObjectPaths(node, node.path);
-      }
-    });
-  };
-  
-
-  return (
-    <Fragment>
-      <DropdownTreeSelect 
-        data={categories} 
-        onChange={onChange}
-        className="treeview-selector" 
-        mode={"radioSelect"}
-        texts={{label: 'Категории'}}  
-      />
-
-
-    </Fragment>
-  )
+  <div></div>;
 }
 
 export default CategorySelect;
