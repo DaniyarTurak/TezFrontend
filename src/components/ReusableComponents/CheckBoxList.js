f56ef3c2e89c929a391a0ededd577810dd183c1f
piko0101 <sonepiko@gmail.com>
2022-02-18 11:37:52 +0600

    add checkedAll

--- src/components/ReusableComponents/CheckBoxList.js
+++ src/components/ReusableComponents/CheckBoxList.js
@@ -1,9 +1,10 @@
 import React, { Fragment, useState, useEffect } from "react";
 import Checkbox from "@mui/material/Checkbox";
 
-const CheckBoxList = ({ category, functions, setAllChecks }) => {
+const CheckBoxList = ({ category, functions, setAllChecks, accessFunctions}) => {
   //console.log("Category: ", category);
   //console.log("Functions: ", functions);<Checkbox onChange={() => setAllChecks()} />
+  const [checkedAll, setCheckedAll] = useState(functions.length == accessFunctions.length)
 
   return (
     <div>
@@ -12,9 +13,12 @@ const CheckBoxList = ({ category, functions, setAllChecks }) => {
           onChange={(e) =>
             setAllChecks(
               e,
-              functions.map((access) => access.key)
+              functions.map((access) => access.key),
+              setCheckedAll
             )
+
           }
+          checked={checkedAll}
         />
         {category}
       </div>
